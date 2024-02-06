e8e8394eff5128c208804ce41dc91b99fdc1d2c2
SrimanUXmint <srimantharun@uxmint.in>
2024-02-06 14:40:28 +0530

    added feedback and feedback-edit

--- src/app/component/feedback-edit/feedback-edit.page.ts
+++ src/app/component/feedback-edit/feedback-edit.page.ts
@@ -2,19 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MenuComponent } from "../menu/menu.component";
+
 
 @Component({
   selector: 'app-feedback-edit',
   templateUrl: './feedback-edit.page.html',
   styleUrls: ['./feedback-edit.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule, MenuComponent]
 })
-export class FeedbackEditPage implements OnInit {
+export class FeedbackEditPage {
+  registerForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      address: ['', Validators.required],
+      profileImage: [''], // For storing the selected image
+    });
   }
 
-}
+  // Function to handle form submission
+  onRegister() {
+    // Access form values using this.registerForm.value
+    console.log('Form submitted:', this.registerForm.value);
+
+
+}}
+
