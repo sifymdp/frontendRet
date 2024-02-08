165d7e7edb64d8ebdd9884c082754b31b40010e2
Dhanvarsha147 <dhanvarsha@uxmint.in>
2024-02-08 12:39:05 +0530

    Theme changes

--- src/app/component/home/home.page.ts
+++ src/app/component/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonMenu, IonModal, IonImg } from '@ionic/angular/standalone';
-import { IonicSlides } from '@ionic/angular';
+import { IonicSlides, ModalController } from '@ionic/angular';
 import { OrderAgainComponent } from 'src/app/component/order-again/order-again.component';
 import { TopDealsComponent } from 'src/app/component/top-deals/top-deals.component';
 import { AddToCartBottomSheetComponent } from 'src/app/component/add-to-cart-bottom-sheet/add-to-cart-bottom-sheet.component';
@@ -22,23 +22,31 @@ export class HomePage {
   bannerImages = [
     {
       id: 1,
-      src: '/assets/imgs/Frame 193.png',
-      alt: 'alt'
+      src: '/assets/imgs/bannerad.png',
+      alt: 'alt',
+      title: 'Big Sale',
+      description: 'Get all your favourites at a discount of upto 50%'
     },
     {
       id: 2,
-      src: '/assets/imgs/Frame 193.png',
-      alt: 'alt'
+      src: '/assets/imgs/bannerad.png',
+      alt: 'alt',
+      title: 'Big Sale',
+      description: 'Get all your favourites at a discount of upto 50%'
     },
     {
       id: 3,
-      src: '/assets/imgs/Frame 193.png',
-      alt: 'alt'
+      src: '/assets/imgs/bannerad.png',
+      alt: 'alt',
+      title: 'Big Sale',
+      description: 'Get all your favourites at a discount of upto 50%'
     },
     {
       id: 4,
-      src: '/assets/imgs/Frame 193.png',
-      alt: 'alt'
+      src: '/assets/imgs/bannerad.png',
+      alt: 'alt',
+      title: 'Big Sale',
+      description: 'Get all your favourites at a discount of upto 50%'
     }
   ]
 
@@ -174,11 +182,70 @@ export class HomePage {
     }
   ]
 
+  showMore:boolean = false;
+  price_details = [
+    {
+      id: '1',
+      price: '200.00',
+      qty: '1 - 99'
+    },
+    {
+      id: '2',
+      price: '150.00',
+      qty: '100 - 499'
+    },
+    {
+      id: '3',
+      price: '126.00',
+      qty: '500 +'
+    }
+  ]
+  order_details = 
+    {
+      id: 1,
+      img: {
+        src: 'assets/imgs/product.png',
+        alt: 'product'
+      },
+      icon: '/assets/imgs/star-unliked.svg',
+      brand_name: 'Fortune',
+      product_name: 'Refined Sunflower Oil',
+      unit_cross_quantity: '20 ut x 500 ml',
+      rate: '₹ 126.00',
+      offer: '45% Off!',
+      uom: 'Each',
+      qty: '40'
+    }
+
   constructor() {}
 
 
   reset(event: any){
+  }
 
+  speechReg(){
+  }
+
+  showMoreDesc(){
+    this.showMore = !this.showMore;
+  }
+
+  add(){
+    // model.dismiss();
+    let msg = document.getElementById("success-message")!;
+    msg.style.display = "block";
+  }
+
+  decrease(){}
+  
+  increase(){}
+
+  toggleLiked(order: any) {
+    if (order.icon === '/assets/imgs/star-liked.svg') {
+      order.icon = '/assets/imgs/star-unliked.svg';
+    } else {
+      order.icon = '/assets/imgs/star-liked.svg';
+    }
   }
 
 }
